c5acfbfdb7bb17a9e2ad5665f0aa693816c631f7
TuiLee <tuchion2018@gmail.com>
2023-05-31 14:46:29 -0300

    Tela de game add

---
+++ Formularios-PDO/game.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/newstyle.css">
+  <title>The Citizen</title>
+</head>
+<body>
+  <div class="logo">
+    <img src="imagens/Banner-1-The Citizen.png" alt="The Citizen">
+  </div>
+  <nav>
+    <ul class="menu">
+      <li>
+        <a href="index.php">Inicio</a>
+      </li>
+      <li>
+        <a href="login.php">Game</a>
+      </li>
+    </ul>
+  </nav>
+  <div class="container">
+    <section>
+      <div class="main">
+        <h1>Citizen</h1>
+        <fieldset>
+
+          <div class="personagem">
+            <div class="imgperfil">
+            <img src="./imagens/img-Perfil 90 90.jpg" alt="Perfil">
+            </div>
+            <div class="info">
+              &nbsp;Nome: Arthur Piazera
+              <hr>
+              &nbsp;Idade: 22
+              <hr>
+              &nbsp;Saude: <a href="" class="saude">Bem</a>
+              <hr>
+              &nbsp;Saldo: <a href="" class="money">1.300,00</a>
+              <hr> 
+              </div>
+              
+          </div>
+          <div class="options">
+            <button class="btn">Perfil</button>
+            <button class="btn">Atributos</button>
+            <button class="btn">Roupa</button>
+            <button class="btn">Negocio</button>
+          </div>
+            <div class="options1">
+            <button class="btn">Coração</button>
+            <button class="btn">Trabalhar</button>
+            <button class="btn">Relógio</button>
+            <button class="btn">Pesquisa</button>
+          </div>
+        </fieldset>
+
+      </div>
+    </section>
+  </div>
+</body>
+</html>
--- Formularios-PDO/home.php
+++ Formularios-PDO/home.php
@@ -25,155 +25,123 @@ session_start();
 </head>
 <body>
     <div class="logo">
-            <img src="imagens/Banner-1-The Citizen.png" alt="Logo">
+        <img src="imagens/Banner-1-The Citizen.png" alt="Logo">
     </div>
-        <nav>
-                <ul class="menu">
-                    <li><a href="index.php">Inicio</a></li><br>
-                    <li><a href="login.php">Game</a></li>
-                </ul>
-            </nav>
-        <div class="container">
+    <nav>
+        <ul class="menu">
+            <li>
+                <a href="index.php">Inicio</a></li><br>
+            <li>
+                <a href="login.php">Game</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="container">
             
-            <section>
-                <div class="cadastre">
-                    <h1> Olá Citizen</h1>
-                    <hr>
-                    <br><br>
-                    <fieldset>
-                        <legend>Login</legend>
-                            <div class="divsair">
-                            <button class="btnsair"><a href="sair.php">X</a></button>
-                            </div>
-                            
-                            <div class="textphp">
-                                <?php
-                                
-                                if (!empty($_SESSION['email'])){
-                                    print "&emsp;Bem vindo,\n".$_SESSION['nome']."\n".$_SESSION['sobrenome'].".<br>";}
-
-                                //switch ($_SESSION['parent1']){
-                                    //case 0:
-                                if($_SESSION['parent1'] == 0 && $_SESSION['parent2'] == 0){
-                                    print "<p>&emsp;Filho de pais desconhecidos."; 
-                                }else if($_SESSION['parent1'] == 0 && $_SESSION['parent2'] !=0){
-                                    print "<p>&emsp;Filho de pai desconhecido e ".trim($fileF[$_SESSION['parent2']]).".";
-                                }else if($_SESSION['parent1'] !=0 && $_SESSION['parent2'] == 0)
-                                {
-                                    print "&emsp;Filho de ".$fileM[$_SESSION['parent1']]."e mãe desconhecida.";
-                                }else{
-                                    print "&emsp;Filho de ".$fileM[$_SESSION['parent1']]."e ".trim($fileF[$_SESSION['parent2']]).".";
-                                }
-                                    /*break;
-                                    case 1:
-                                        print "<p>&emsp;Filho de Rodrigo e ";
-                                    break;
-                                    case 2:
-                                        print "<p>&emsp;Filho de Bruno e ";
-                                    break;
-                                    case 3:
-                                        print "<p>&emsp;Filho de Carlos e ";
-                                    break;
-                                    case 4:
-                                        print "<p>&emsp;Filho de Yan e ";
-                                    break;
-                                    case 5:
-                                        print "<p>&emsp;Filho de Arthur e ";
-                                    break;*/
-                                //}
-                                /*switch ($_SESSION['parent2']){
-                                    case 0:
-                                        if($_SESSION['parent1'] == 0 && $_SESSION['parent2'] == 0){
-                                            print ".</p>"; 
-                                        }else{
-                                            print "mãe desconhecida.</p>";}
-                                    break;
-                                    case 1:
-                                        print "Gabriela.</p>";
-                                    break;
-                                    case 2:
-                                        print "Brenda.</p>";
-                                    break;
-                                    case 3:
-                                        print "Ana.</p>";
-                                    break;
-                                    case 4:
-                                        print "Carolina.</p>";
-                                    break;
-                                    case 5:
-                                        print "Daniela.</p>";
-                                    break;
-                                }*/
-                                if ($_SESSION['parentAdotivo1']!=null && $_SESSION['parentAdotivo2']!=null){
-                                    switch($_SESSION['parentAdotivo1']){
-                                        case 1:
-                                            print "&emsp;Pais adotivos, Rodrigo e ";
-                                        break;
-                                        case 2:
-                                            print "&emsp;Pais adotivos, Bruno e ";
-                                        break;
-                                        case 3:
-                                            print "&emsp;Pais adotivos, Carlos e ";
-                                        break;
-                                        case 4:
-                                            print "&emsp;Pais adotivos, Yan e ";
-                                        break;
-                                        case 5:
-                                            print "&emsp;Pais adotivos, Arthur e ";
-                                        break;
-                                    }
-                                    switch($_SESSION['parentAdotivo2']){
-                                        case 1:
-                                            print "Gabriela.";
-                                        break;
-                                        case 2:
-                                            print "Brenda.";
-                                        break;
-                                        case 3:
-                                            print "Ana.";
-                                        break;
-                                        case 4:
-                                            print "Carolina.";
-                                        break;
-                                        case 5:
-                                            print "Daniela.";
-                                        break;
-                                    }
-                                }  
-                                switch ($_SESSION['locinicio']){
-                                    case 0:
-                                        print "<p>&emsp;Nascido de parto normal esta morando com seus pais.</p>";
-                                    break;
-                                    case 1:
-                                        print "<p>&emsp;Sofre um acidente de avião e se encontra em uma ilha aparentemente deserta.</p>";
-                                    break;
-                                    case 2:
-                                        print "<p>&emsp;Foi adotado ainda jovem e mora com eles.</p>";
-                                    break;
-                                    case 3:
-                                        print "<p>&emsp;Foi encontrado na porta de uma igreja e entrege a seus pais adotivos que o acolheram.</p>";
-                                    break;
-                                    case 4:
-                                        print "<p>&emsp;Fruto de uma inceminação artificial mora com seus pais.</p>";
-                                    break;
-                                    
-                                }   
-
-                                
-                                ?>
-                            </div>
-                            <br>
-                    </fieldset>
-                </div>
-                    
+        <section>
+            <div class="cadastre">
+                <h1> Olá Citizen</h1>
+                <hr>
                 <br><br>
-            </section>
-            <div class="options">
-                <button class="vamos">
-                    Ok,Vamos
-                </button>
-            </div>
+                <fieldset>
+                <legend>Login</legend>
+                    <div class="divsair">
+                        <button class="btnsair"><a href="sair.php">X</a></button>
+                    </div>
+                            
+                <div class="textphp">
+                    <?php
+                                
+                        if (!empty($_SESSION['email'])){
+
+                            print "&emsp;Bem vindo,\n".$_SESSION['nome']."\n".$_SESSION['sobrenome'].".<br>";
+
+                        }
+                        if($_SESSION['parent1'] == 0 && $_SESSION['parent2'] == 0){
+
+                            print "<p>&emsp;Filho de pais desconhecidos."; 
+
+                        }else if($_SESSION['parent1'] == 0 && $_SESSION['parent2'] !=0){
+
+                            print "<p>&emsp;Filho de pai desconhecido e ".trim($fileF[$_SESSION['parent2']]).".";
+
+                        }else if($_SESSION['parent1'] !=0 && $_SESSION['parent2'] == 0){
+
+                            print "&emsp;Filho de ".$fileM[$_SESSION['parent1']]."e mãe desconhecida.";
+
+                        }else{
+
+                            print "&emsp;Filho de ".$fileM[$_SESSION['parent1']]."e ".trim($fileF[$_SESSION['parent2']]).".";
+
+                        }
+                        if ($_SESSION['parentAdotivo1']!=null && $_SESSION['parentAdotivo2']!=null){
+                            switch($_SESSION['parentAdotivo1']){
+                                case 1:
+                                    print "&emsp;Pais adotivos, Rodrigo e ";
+                                break;
+                                case 2:
+                                    print "&emsp;Pais adotivos, Bruno e ";
+                                break;
+                                case 3:
+                                    print "&emsp;Pais adotivos, Carlos e ";
+                                break;
+                                case 4:
+                                    print "&emsp;Pais adotivos, Yan e ";
+                                break;
+                                case 5:
+                                    print "&emsp;Pais adotivos, Arthur e ";
+                                break;
+                            }
+                            switch($_SESSION['parentAdotivo2']){
+                                case 1:
+                                    print "Gabriela.";
+                                break;
+                                case 2:
+                                    print "Brenda.";
+                                break;
+                                case 3:
+                                    print "Ana.";
+                                break;
+                                case 4:
+                                    print "Carolina.";
+                                break;
+                                case 5:
+                                    print "Daniela.";
+                                break;
+                            }
+                        }  
+
+                        switch ($_SESSION['locinicio']){
+                            case 0:
+                                print "<p>&emsp;Nascido de parto normal esta morando com seus pais.</p>";
+                            break;
+                            case 1:
+                                print "<p>&emsp;Sofre um acidente de avião e se encontra em uma ilha aparentemente deserta.</p>";
+                            break;
+                            case 2:
+                                print "<p>&emsp;Foi adotado ainda jovem e mora com eles.</p>";
+                            break;
+                            case 3:
+                                print "<p>&emsp;Foi encontrado na porta de uma igreja e entrege a seus pais adotivos que o acolheram.</p>";
+                            break;
+                            case 4:
+                                print "<p>&emsp;Fruto de uma inceminação artificial mora com seus pais.</p>";
+                            break;
+                                    
+                        } 
+                    ?>
+                </div>
+                <br>
+                </fieldset>
+            </div>   
+
+            <br><br>
+
+        </section>
+        <div class="options">
+            <button class="vamos"><a href="game.php">Vamos, lá</a></button>
         </div>
+    </div>
     
 </body>
 </html>
